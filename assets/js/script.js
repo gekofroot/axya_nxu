f4b9ecea04cf63742070e741e3f8d91023bf7dbd
gekofroot <99528330+gekofroot@users.noreply.github.com>
2026-02-25 18:43:38 -0700

    fresh commit

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+
+let search = document.getElementById('search')
+let ulItem = document.getElementById('ul-item')
+let ulListItems = document.getElementsByTagName('li')
+let ulListItemsA = document.getElementsByTagName('a')
+
+
+// element load in
+for (let x = 0; x < ulListItems.length; x++) {
+  ulListItems[x].style.opacity = '0'
+  ulListItems[x].style.transition = '.4s'
+  ulListItems[x].style.borderBottom = '0 outset var(--acnt-b)'
+}
+
+setTimeout(() => {
+  let itemLoadCount = 0
+  let itemLoadInterval = setInterval(() => {
+    if (itemLoadCount === ulListItems.length) {
+      for (let x = 0; x < ulListItems.length; x++) {
+	ulListItems[x].style.transition = '.2s'
+	ulListItems[x].style.borderBottom = '3px outset var(--acnt-b)'
+      }
+      clearInterval(itemLoadInterval)
+    } else {
+      ulListItems[itemLoadCount].style.opacity = '100%'
+      itemLoadCount += 1
+    }
+  }, 20)
+}, 100)
+
+search.addEventListener('keyup', () => {
+  let eventKey = event.key
+  for (let x = 0; x < ulListItems.length; x++) {
+    let currentSearchValue = search.value.toLowerCase()
+    let currentUlItem = ulListItemsA[x].innerHTML.toLowerCase()
+    if (currentUlItem.includes(currentSearchValue)) {
+      ulListItems[x].style.opacity = '100%'
+      setTimeout(() => {
+	ulListItems[x].style.display = 'flex'
+      }, 200)
+    } else {
+      ulListItems[x].style.opacity = '0'
+      setTimeout(() => {
+	ulListItems[x].style.display = 'none'
+      }, 200)
+    }
+  }
+})
+
